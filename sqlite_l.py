e6c91d6a8b5ae6b5593d2d68d9550a57623b92f5
dev-vickie <vmutethia84@gmail.com>
2024-09-20 12:02:37 +0300

    learn: parametrized statements

--- sqlite_l.py
+++ sqlite_l.py
@@ -1,24 +1,20 @@
 import sqlite3
 
-with sqlite3.connect("test_database.db") as connection:
+data = (
+    ("Jean-Baptise Zorg","Human",122),
+    ("Korben Dallas","Meat Popsicle",100),
+    ("Ak'not","Mangalore",-5)
+        )
+
+with sqlite3.connect("another_test.db") as connection:
     cursor = connection.cursor()
-    cursor.executescript(
-        """
-        CREATE TABLE People(
-            FirstName TEXT,
-            LastName TEXT,
-            Age INT
-        );
-        INSERT INTO People VALUES(
-        'Vic',
-        'Max',
-        23
-        );
-        """
-    )
+    cursor.execute(
+        """CREATE TABLE Roster(
+        Name TEXT,
+        Species TEXT,
+        IQ INT
+        );"""
+            )
 
-# No connection.commit() or connection.close()
-# All changes are automatically commited when the 'with' block is done executing
+    cursor.executemany("INSERT INTO Roster VALUES(?,?,?)",data)
 
-
-    
